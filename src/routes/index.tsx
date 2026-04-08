30667c8d40d513b19d5e10d8a8062e3df4a5c2c0
Jesse Gibson <JesseTheGibson@gmail.com>
2026-04-08 18:36:34 -0400

    Replace TODO placeholder with landing page
    Uses the new Text and Heading components from #ui.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,14 @@
+import { Heading, Text } from '#ui';
+
 export default function Home() {
-  return 'TODO';
+  return (
+    <main>
+      <Heading as="h1" size={7}>
+        Apps
+      </Heading>
+      <Text as="p" color="lowContrast">
+        Work in progress
+      </Text>
+    </main>
+  );
 }
